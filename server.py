26e31319e8317eb63262b63730d58260f20c6395
brandt.shmuel@gmail.com <root@Mooliz-laptop>
2024-09-10 09:59:07 +0300

    Updated server code to support multi-threading

--- server.py
+++ server.py
@@ -1,22 +1,60 @@
 import socket
 import argparse
 import sys
+from threading import Thread
 
 
-def run_server(ip, port):
+def receiver(conn):
+    from_client = ""
+    while True:
+        data = conn.recv(4096)
+        if not data:
+            break
+        from_client += data.decode('utf8')
+    print(f'Received data: {from_client}')
+
+
+def run_server(client_ip, client_port):
+    """
+    Receive data from client at address (ip, port).
+    """
     serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serv.bind((ip, port))
+    serv.bind((client_ip, client_port))
     serv.listen(5)
     while True:
         conn, addr = serv.accept()
-        from_client = ''
-        while True:
-            data = conn.recv(4096)
-            if not data:
-                break
-            from_client += data.decode('utf8')
-            print(f'Received data: {from_client}')
+
+        t = Thread(target=receiver, args=[conn])
+        t.start()
+        print(f"Message from thread {t.native_id}.")
+        #t.run()
+        t.join()
         conn.close()
+
+        #receiver(conn)
+        #conn.close()
+
+
+def connections():
+    """
+    Handle connection requests from clients
+    """
+    while True:
+        args = get_args()
+        try:
+            """
+            t = Thread(target=run_server, args=[args.client_ip, args.client_port])
+            t.start()
+            t.run()
+            print(f"Message from thread {t.native_id}.")
+            t.join()
+            print("Done.")
+            """
+            run_server(args.client_ip, args.client_port)
+            print("Done.")
+        except Exception as error:
+            print(f'ERROR: {error}')
+            return 1
 
 
 def get_args():
@@ -32,13 +70,7 @@ def main():
     """
     Implementation of CLI and sending data to server.
     """
-    args = get_args()
-    try:
-        run_server(args.client_ip, args.client_port)
-        print('Done.')
-    except Exception as error:
-        print(f'ERROR: {error}')
-        return 1
+    connections()
 
 
 if __name__ == '__main__':
